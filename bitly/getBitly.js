06665e50211285d56701fc0dc784f3ea83c93a59
Brandon <brandoncyu@gmail.com>
2019-07-14 09:48:50 -0700

    added bitly things

--- bitly/getBitly.js
+++ bitly/getBitly.js
@@ -1,23 +1,32 @@
-require('dotenv').config();
-const $ = require('jquery');
+// require('dotenv').config();
+const axios = require('axios')
+// const $ = require('jquery');
 
-const getBitlyLink = (long_url, login, api_key, func) => {
-    $.getJSON(
-        "http://api.bitly.com/v3/shorten?callback=?", 
-        { 
-            "format": "json",
-            "apiKey": api_key,
-            "login": login,
-            "longUrl": long_url
-        },
-        (response) => func(response.data.url)
-    );
+const getBitlyLink = async (long_url, api_key, func) => {
+    console.log('work')
+    try {
+        const response = await axios(
+            "https://api-ssl.bitly.com/v4/shorten",
+            {
+                method: 'POST',
+                headers: {
+                    'Host': 'api-ssl.bitly.com',
+                    'Authorization': `Bearer ${api_key}`,
+                    'Content-Type': 'application/json'
+                },
+                data: {
+                    group_guid: "string",
+                    long_url: long_url
+                },
+            }
+        );
+        return response
+    } catch(e){
+        console.log(e)
+    }
 }
 
-const login = "LOGIN_HERE";
-const api_key = process.env.BITLY_API;
+const token = "3bd5246b8f53327260d0324c6bc570093b1f8980";
 const long_url = "http://www.google.com/";
 
-getBitlyLink(long_url, login, api_key, function(short_url) {
-    console.log(short_url);
-});
+getBitlyLink(long_url, token);
